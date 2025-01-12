881941c9746998a6454e7cfe95bd263086229f1a
Elham <elhamwaheed777@gmail.com>
2025-01-13 03:12:19 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Fetch and Store the Date
+function generate() {
+  const saveData = {
+    myFile: document.getElementById("myFile").value,
+    firstName: document.getElementById("firstName").value,
+    secondName: document.getElementById("secondName").value,
+    qualification: document.getElementById("qualification").value,
+    email: document.getElementById("email").value,
+  };
+  // console.log(saveData.firstName);
+  // console.log(saveData.secondName);
+  // console.log(saveData.qualification);
+  // console.log(saveData.email);
+
+}
+

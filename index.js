898c68d3bb689e6ce7ee505ab2c940e5cd01661c
Elham <elhamwaheed777@gmail.com>
2025-01-13 07:29:18 +0500

    Second Commit

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function generate() {
     secondName: document.getElementById("secondName").value,
     qualification: document.getElementById("qualification").value,
     email: document.getElementById("email").value,
+    description: document.getElementById("description").value,
   };
-  // console.log(saveData.firstName);
-  // console.log(saveData.secondName);
-  // console.log(saveData.qualification);
-  // console.log(saveData.email);
-
+  // Save into Local Storage
+  localStorage.setItem("userData", JSON.stringify(saveData));
+  // Navigate into Template Page
+  window.location.href = "Template/temp.html";
 }
-
+export { generate };
